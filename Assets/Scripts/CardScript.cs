2df2ccb1bdabb7d5b8f4e41a8335a365136e9f11
ivandanny <danny.ivan@yahoo.com>
2017-12-08 23:22:35 +0800

    Initiation

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour {
+    public ManagerScript managerscript;
+    public int attack;
+    public int hp;
+	// Use this for initialization
+	void Start () {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void TaskOnClick()
+    {
+        Debug.Log("You Have Clicked Me!");
+    }
+}
